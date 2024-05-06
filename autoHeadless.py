25ff2ffdd7dabe393929d98763459d62b144b459
Daniel Sifton <Daniel.Sifton@viu.ca>
2024-05-06 14:40:17 -0700

    switch to Firefox

--- autoHeadless.py
+++ autoHeadless.py
@@ -1,67 +1,53 @@
 #python 3.11.0
-#headlessly
+#headlessly, Firefox
 import time
 import csv
-from itertools import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 
+# Set up headless Firefox
+firefox_options = Options()
+firefox_options.add_argument("--headless")  # Run in headless mode
 
-chrome_options = Options()
-chrome_options.add_argument("--headless=chrome")
-driver = webdriver.Chrome(service=Service('PATH TO YOUR CHROMEDRIVER'),
-                          options=chrome_options)
+# Initialize the Firefox driver
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=firefox_options)
 
-
+# Prompt for the CSV file name and open it
 csvfile = input("Enter the name of the csv input file . . .")
-
 urls = csv.reader(open(csvfile, "r"))
 count = 1
 
-
-
-
-
+# Process each URL in the CSV
 for url in urls:
-	
+    driver.get("YOUR SERVER URL HERE")
 
-		driver.get("URL FOR YOUR SERVER")
+#using FF inspect => xpath here :) 
 
-		thread_link = driver.find_element(By.ID, "url")
+    thread_link = driver.find_element(By.ID, "url")
+    thread_link.send_keys(url[0])
 
-		thread_link.send_keys(url[0])
+    reason = driver.find_element(By.ID, "why")
+    reason.send_keys(url[0])
 
-		reason = driver.find_element(By.ID, "why")
-		reason.send_keys(url[0])
+    checkbox = driver.find_element(By.ID, "unfold-thread")
+    checkbox.click()
 
-		checkbox = driver.find_element(By.ID, "unfold-thread")
-		checkbox.click()
+    time.sleep(10)
 
-		time.sleep(10)
+    capture_button = driver.find_element(By.XPATH, "/html/body/main/form/fieldset[3]/button")
+    capture_button.click()
 
+    time.sleep(10)
 
-		capture_button = driver.find_element(By.XPATH, value="/html/body/main/form/fieldset[3]/button")
-		#using FF inspect => xpath here :) 
-		capture_button.click()
+    final_capture_button = driver.find_element(By.XPATH, "/html/body/main/dialog/button")
+    final_capture_button.click()
 
-		time.sleep(10)
+    time.sleep(40)
 
-		final_capture_button = driver.find_element(By.XPATH, value="/html/body/main/dialog/button")
-		final_capture_button.click()
+    print(count, url[0])
+    count += 1
 
-		time.sleep(40)
-		
-		#driver.quit()
-
-		print(count , url[0])
-		#print(url[0])
-		#print(count)
-		count += 1
-		
-	
-		
-
-
+# driver.quit()  # Uncomment to close the browser when done
